e8e0a306d385cfe4ed4588a754bb8653ab423e2d
karlagutz <karlagutz07@gmail.com>
2015-11-22 15:22:38 -0700

    🌚

--- gen/com/example/reportes2/R.java
+++ gen/com/example/reportes2/R.java
@@ -24,16 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000e;
         public static final int btEntrar=0x7f080009;
         public static final int email=0x7f080007;
         public static final int email_login_form=0x7f080006;
+        public static final int lblListHeader=0x7f08000c;
+        public static final int lblListItem=0x7f08000d;
         public static final int ll1=0x7f080001;
         public static final int ll2=0x7f080003;
         public static final int ll3=0x7f08000a;
         public static final int login_form=0x7f080005;
         public static final int login_progress=0x7f080004;
-        public static final int lvMaterias=0x7f080000;
+        public static final int lvAula=0x7f080000;
         public static final int password=0x7f080008;
         public static final int textView2=0x7f080002;
         public static final int tvRegistrar=0x7f08000b;
@@ -41,6 +43,8 @@ public final class R {
     public static final class layout {
         public static final int activity_aula=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int list_group=0x7f030002;
+        public static final int list_item=0x7f030003;
     }
     public static final class menu {
         public static final int aula=0x7f070000;
--- src/com/example/reportes2/Aula.java
+++ src/com/example/reportes2/Aula.java
@@ -1,22 +1,154 @@
 package com.example.reportes2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
-public class Aula extends Activity {
 
+public class Aula extends Activity implements OnChildClickListener {
+	ExpandableListAdapter listAdapter;
+	ExpandableListView expListView;
+	List<String> listDataHeader;
+	HashMap<String, List<String>> listDataChild;
+	Context con = this;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_aula);
+		
+		 expListView = (ExpandableListView) findViewById(R.id.lvAula);
+	
+		 // preparing list data
+	        prepareListData();
+	 
+	        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+	 
+	        // setting list adapter
+	        expListView.setAdapter(listAdapter);
+	     
+	        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+	        	 
+	            @Override
+	            public boolean onGroupClick(ExpandableListView parent, View v,
+	                    int groupPosition, long id) {
+	                // Toast.makeText(getApplicationContext(),
+	                // "Group Clicked " + listDataHeader.get(groupPosition),
+	                // Toast.LENGTH_SHORT).show();
+	                return false;
+	            }
+	        });
+	        
+	        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+	        	 
+	            @Override
+	            public void onGroupExpand(int groupPosition) {
+	                Toast.makeText(getApplicationContext(),
+	                        listDataHeader.get(groupPosition) + " Expanded",
+	                        Toast.LENGTH_SHORT).show();
+	            }
+	        });
+	        
+	     // Listview Group collasped listener
+	        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+	 
+	            @Override
+	            public void onGroupCollapse(int groupPosition) {
+	                Toast.makeText(getApplicationContext(),
+	                        listDataHeader.get(groupPosition) + " Collapsed",
+	                        Toast.LENGTH_SHORT).show();
+	 
+	            }
+	        });
+	        
+	        // Listview on child click listener
+	        expListView.setOnChildClickListener(new OnChildClickListener() {
+	 
+	            @Override
+	            public boolean onChildClick(ExpandableListView parent, View v,
+	                    int groupPosition, int childPosition, long id) {
+	                // TODO Auto-generated method stub
+	                Toast.makeText(
+	                        getApplicationContext(),
+	                        listDataHeader.get(groupPosition)
+	                                + " : "
+	                                + listDataChild.get(
+	                                        listDataHeader.get(groupPosition)).get(
+	                                        childPosition), Toast.LENGTH_SHORT)
+	                        .show();
+	                return false;
+	            }
+	        });
+
 	}
+	
+	  /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+ 
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+ 
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+ 
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+ 
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+ 
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.aula, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onChildClick(ExpandableListView arg0, View arg1, int arg2,
+			int arg3, long arg4) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
